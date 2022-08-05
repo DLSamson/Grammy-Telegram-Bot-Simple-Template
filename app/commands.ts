25edc39537862b6fc2f75a474919b378baa4e57d
dgarmash <dgarmash@synergy.ru>
2022-08-05 09:34:09 +0300

    Making a template

--- app/commands.ts
+++ app/commands.ts
@@ -1,11 +1,14 @@
+import { Context } from 'grammy';
 import {bot} from './bot';
 
 bot.command('start', ctx => {
-    const message = "Welcome! 🖐️\n"
-    + "Here, you can play game Bulls&Cows\n";
+    const message = 'Hello, this is bot template!';
     ctx.reply(message);
 });
 
-bot.command('help', ctx => {
+
+const help = (ctx:Context) => {
     ctx.reply('How can I help you?');
-});
+};
+bot.command('help', help);
+bot.callbackQuery('help', help);
